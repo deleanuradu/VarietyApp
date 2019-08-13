a5dba63e2521ccc88140041c54419b25304497c5
neagugeorgecristian <neagugeorge998@gmail.com>
2019-08-13 14:27:52 +0300

    Activity Template cu floating button

--- app/src/main/java/com/inversion/varietyapp/MasterListActivity.java
+++ app/src/main/java/com/inversion/varietyapp/MasterListActivity.java
@@ -1,8 +1,10 @@
 package com.inversion.varietyapp;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MasterListActivity extends AppCompatActivity {
@@ -11,9 +13,17 @@ public class MasterListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_masterlist);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
-
 }
-
-
