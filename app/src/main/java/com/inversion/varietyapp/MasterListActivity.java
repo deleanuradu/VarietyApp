4a190ff1e785c558ad841574f9ee2951fd94c113
neagugeorgecristian <neagugeorge998@gmail.com>
2019-08-13 12:57:34 +0300

    a

--- app/src/main/java/com/inversion/varietyapp/MasterListActivity.java
+++ app/src/main/java/com/inversion/varietyapp/MasterListActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MasterListActivity {
+public class MasterListActivity extends MainActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
