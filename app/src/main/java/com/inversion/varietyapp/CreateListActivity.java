77dfde346e751cf97e7b96dbcf732d4f10efe6c8
neagugeorgecristian <neagugeorge998@gmail.com>
2019-08-13 15:58:14 +0300

    Main, MasterList, Create List începute

--- app/src/main/java/com/inversion/varietyapp/CreateListActivity.java
+++ app/src/main/java/com/inversion/varietyapp/CreateListActivity.java
@@ -1,4 +1,34 @@
 package com.inversion.varietyapp;
 
-public class CreateListActivity {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateListActivity extends AppCompatActivity {
+
+    EditText title;
+    EditText description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_list);
+
+        title = findViewById(R.id.edit_title);
+        description = findViewById(R.id.edit_description);
+    }
+
+    public void resetFields(View view) {
+        title.setText("");
+        description.setText("");
+    }
+
+    public void savePreferences(View view) {
+        String s = title.getText().toString();
+        Intent intent = new Intent(this, MasterListActivity.class);
+        intent.putExtra("cheie",s);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/inversion/varietyapp/MasterListActivity.java
+++ app/src/main/java/com/inversion/varietyapp/MasterListActivity.java
@@ -1,13 +1,17 @@
 package com.inversion.varietyapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class MasterListActivity extends AppCompatActivity {
+
+    TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,20 @@ public class MasterListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), CreateListActivity.class);
+                startActivity(intent);
             }
         });
+
+        text1 = findViewById(R.id.text1);
+
+        Intent receivedIntent = getIntent();
+        if (getIntent().getExtras() != null) {
+            String primit = receivedIntent.getStringExtra("cheie");
+            text1.setText(primit);
+            text1.setVisibility(View.VISIBLE);
+        }
+
     }
 
 }
