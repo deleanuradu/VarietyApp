4c9ef897d57e57427c131768a6a5e1a330d2b3dc
numeprenume <victrusu@gmail.com>
2019-08-29 12:18:37 +0300

    Migrated to AndroidX, updated dependencies and SDK versions

--- app/src/main/java/com/inversion/varietyapp/MainActivity.java
+++ app/src/main/java/com/inversion/varietyapp/MainActivity.java
@@ -1,10 +1,9 @@
 package com.inversion.varietyapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
